58b269b6d728f8af7cad7e6741786ab25e008e08
Giovax0Fail <g.ferrante5@outlook.com>
2024-09-24 22:38:12 +0200

    a

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
 let humanScore = 0;
 let machineScore = 0;
+
 function getComputerChoice() {
-    let number = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-    console.log(number);
+    let number = Math.floor(Math.random() * 3) + 1;
+    return NumberToString(number);
+}
+
+function NumberToString(number) {
+    let ComChoice = "";
+    switch (number) {
+        case 1:
+            ComChoice = "rock";
+            break;
+        case 2:
+            ComChoice = "paper";
+            break;
+        case 3:
+            ComChoice = "scissors";
+            break;
+    }
+    return ComChoice;
 }
 
 function getHumanChoice() {
     let userValue = String(
-        window.prompt("Choose between Rock, Paper, Scissors ", "")
-    );
+        window.prompt("Choose between 'Rock', 'Paper', 'Scissors' ", "")
+    ).toLowerCase();
+    return userValue;
 }
-getComputerChoice();
 
 function playRound(humanChoice, computerChoice) {
-    // your code here!
-}
+    if (humanChoice === computerChoice) 
+        window.alert("Draw!");
+    } 
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+playRound(humanSelection, computerSelection)
